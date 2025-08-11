3c720da8153214911ef31b3e15bf1aabbf1ab23f
ultra <tt.soccer.022484@gmail.com>
2025-08-11 18:03:51 +0900

    first commit
    first commit

---
+++ src/main/java/com/example/my_sample_api/MySampleApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.my_sample_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySampleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySampleApiApplication.class, args);
+	}
+
+}
